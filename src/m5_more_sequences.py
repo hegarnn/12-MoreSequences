408c99c6fa741fe711f6826d085e50d2bb8f3d5e
hegarnn <hegarnn@rose-hulman.edu>
2019-01-07 13:50:14 -0500

    123456789

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -306,7 +306,7 @@ def contains_an_a(s):
       :rtype: bool
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
